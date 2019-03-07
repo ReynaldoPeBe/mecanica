07a92d3fb1f66b7bc46009dcdb6de87de34f3190
Jes <Jes@192.168.64.1>
2019-03-07 13:20:14 -0400

    Iniciando el proyecto de mecanica

---
+++ src/main/java/com/umsa/mec/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.umsa.mec.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping(value="/index")
+	public String index (Map<String, Object> m) {
+		m.put("titulo", "Sistema de Inscripcion Mecanica");
+		return"index";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mecanica</title>
+</head>
+<body>Bienvenido al ${titulo}
+</body>
+</html>
